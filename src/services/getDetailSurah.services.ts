9e14b64f7f2b8502a45bbf27139c8e1f6876edb5
aMand7 <amanddseventaurus@gmail.com>
2024-04-15 17:17:28 +0700

    memperbaiki konfigurasi domain image

--- src/services/getDetailSurah.services.ts
+++ src/services/getDetailSurah.services.ts
@@ -2,7 +2,7 @@ import instanceApi from '@/lib/axios';
 
 const getDetailSurah = async (surah: string) => {
   try {
-    const { data } = await instanceApi.get(`/api/v2/surat/${surah}`);
+    const { data } = await instanceApi.get(`/surat/${surah}`);
     return data;
   } catch (error) {
     throw error;
--- src/services/getTafsirSurah.services.ts
+++ src/services/getTafsirSurah.services.ts
@@ -2,7 +2,7 @@ import instanceApi from '@/lib/axios';
 
 const getTafsirSurah = async (surah: string) => {
   try {
-    const { data } = await instanceApi.get(`/api/v2/tafsir/${surah}`);
+    const { data } = await instanceApi.get(`/tafsir/${surah}`);
     return data;
   } catch (error) {
     throw error;
